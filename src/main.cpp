9a3ed4904e07b60c334796710683397e763dc806
ROHAN RATHEE <rohanratheethebest2009@gmail.com>
2026-05-11 09:18:09 +0530

    Update VS Code configs and main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,6 @@ const unsigned int SCR_HEIGHT = 600;
 
 int main()
 {
-
-
     glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -27,8 +25,6 @@ int main()
 #ifdef __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
-
-
 
     GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
@@ -40,25 +36,20 @@ int main()
     glfwMakeContextCurrent(window);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-
-
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
         std::cout << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-
-
-    Shader ourShader("Shaders/vertex.glsl", "Shaders/fragment.glsl");
-
+    Shader ourShader("Shaders/vertex.glsl", "Shaders/fragment.glsl"); 
 
 
     float vertices[] = {
 
-         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
+         0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  
+         0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   
     };
 
     unsigned int VBO, VAO;
@@ -70,29 +61,19 @@ int main()
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
     glEnableVertexAttribArray(1);
 
-
-
-
-
-
     int width, height, nrChannels;
     unsigned char *data = stbi_load("Textures/container.jpg", &width, &height, &nrChannels, 0);
-
-
 
     while (!glfwWindowShouldClose(window))
     {
 
-
         processInput(window);
-
 
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
@@ -103,23 +84,18 @@ int main()
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
-
-
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
 
 
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
 
 
-
     glfwTerminate();
     return 0;
 }
-
 
 
 void processInput(GLFWwindow *window)
@@ -129,10 +105,8 @@ void processInput(GLFWwindow *window)
 }
 
 
-
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-
 
     glViewport(0, 0, width, height);
 }
